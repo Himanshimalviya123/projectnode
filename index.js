ffa54fdf601afcd77fb3ec4495fe957081603dd4
Himanshimalviya123 <malviyahimanshi14@gmail.com>
2025-07-29 20:45:08 +0530

    update

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const express=require("express");
+const express=require("express")
 const app=express();
 const student=require("./routes/sturoutes");
 const mongoose=require("mongoose")
 const bodyparser=require("body-parser");
 app.set("view engine","ejs");
-mongoose.connect("mongodb://127.0.0.1:27017/shivdb").then(()=>{
+mongoose.connect("mongodb://127.0.0.1:27017/himanshidb").then(()=>{
     console.log("Database connected successfuly");
 })
 
@@ -14,6 +14,6 @@ app.use(bodyparser.json());
 
 app.use("/",student)
 
-app.listen(2000,()=>{
-    console.log("server is running on port 2000");
+app.listen(5000,()=>{
+    console.log("server is running on port 5000");
 })
