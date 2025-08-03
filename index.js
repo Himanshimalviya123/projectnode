66c4331d2052207cf7bb078019855a144c225bcb
Himanshimalviya123 <malviyahimanshi14@gmail.com>
2025-08-03 23:02:43 +0530

    update

--- index.js
+++ index.js
@@ -7,13 +7,10 @@ app.set("view engine","ejs");
 mongoose.connect("mongodb://127.0.0.1:27017/himanshidb").then(()=>{
     console.log("Database connected successfuly");
 })
-
 //Bodyparser middleware
 app.use(bodyparser.urlencoded({extended:true}));
 app.use(bodyparser.json());
-
 app.use("/",student)
-
 app.listen(5000,()=>{
     console.log("server is running on port 5000");
 })
